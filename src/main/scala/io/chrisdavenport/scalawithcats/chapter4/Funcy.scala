8ba761ecc9b9ec7664a30c94a74e5bcbb5a8b864
Chris Davenport <chris@christopherdavenport.tech>
2018-02-12 10:50:20 -0500

    Add Secondary Monad Implementation

--- src/main/scala/io/chrisdavenport/scalawithcats/chapter4/Funcy.scala
+++ src/main/scala/io/chrisdavenport/scalawithcats/chapter4/Funcy.scala
@@ -7,4 +7,28 @@ object Funcy {
         def map[A, B](fa: F[A])(f: A => B): F[B] = flatMap(fa)(a => pure(f(a)))
     }
 
+    trait Monad2[F[_]]{
+        def join[A](fa: F[F[A]]): F[A]
+        def map[A, B](fa: F[A])(f1: A => B): F[B]
+
+        def flatMap[A, B](fa: F[A])(f2: A => F[B]): F[B] = {
+            val ffb: F[F[B]] = map(fa)(f2)
+            join(ffb)
+        }
+    }
+
+    object Monad2Instances {
+        val Monad2Option : Monad2[Option] = new Monad2[Option]{
+
+            override def join[A](fa: Option[Option[A]]): Option[A] = fa match {
+                case Some(Some(a)) => Some(a)
+                case Some(None) => None
+                case None => None 
+            }
+
+            override def map[A, B](fa: Option[A])(f1: A => B): Option[B] = fa.map(f1)
+        }
+
+    }
+
 }
