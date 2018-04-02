ce06f24dfe2750ac45688f7365da6fd2c99bf74f
Chris Davenport <chris@christopherdavenport.tech>
2018-04-02 11:05:06 -0400

    Monday Reference Chapter 8

--- src/main/scala/io/chrisdavenport/scalawithcats/chapter8/UptimeClient.scala
+++ src/main/scala/io/chrisdavenport/scalawithcats/chapter8/UptimeClient.scala
@@ -1,7 +1,8 @@
 package io.chrisdavenport.scalawithcats.chapter8
 
 import cats._
-// import cats.implicits._
+import cats.implicits._
+import cats.effect._
 
 trait UptimeClient[F[_]]{
   def getUptime(hostname: String): F[Int]
@@ -10,9 +11,29 @@ trait UptimeClient[F[_]]{
 object UptimeClient{
   def apply[F[_]](implicit ev: UptimeClient[F]) = ev
 
-  def uptimeServiceId(map: Map[String, Int]): UptimeClient[Id] = new UptimeClient[Id]{
-    def getUptime(hostname: String): Id[Int] = map.getOrElse(hostname, 0)
+  def getFromServer(hostname: String): IO[Int] = IO{
+    println(s"$hostname")
+    3
   }
+
+  // Monad
+  // F[A] => (A => F[B]) => F[B]
+  // Comonad
+  // F[B] => (F[B] => A) => F[A]
+
+  // flatten: F[F[A]] => F[A] flatten 
+  // trait Arrow[F[_], G[_]]{
+  //   def nat[A](f: F[A]): G[A] 
+  // }
+
+  // coflatten : F[A] => F[F[A]]
+  // pure: A => F[A]
+  //       F[A] => A
+
+
+  // def uptimeServiceId(map: Map[String, Int]): UptimeClient[Id] = new UptimeClient[Id]{
+  //   def getUptime(hostname: String): Id[Int] = map.getOrElse(hostname, 0)
+  // }
 }
 
 trait UptimeService[F[_]]{
@@ -21,4 +42,8 @@ trait UptimeService[F[_]]{
 
 object UptimeService {
   def apply[F[_]](implicit ev: UptimeService[F]) = ev
+  def impl[F[_]: UptimeClient: Applicative]: UptimeService[F] = new UptimeService[F]{
+    override def getTotalUptime(hostnames: List[String]): F[Int] = 
+      hostnames.traverse(UptimeClient[F].getUptime).map(_.sum)
+  }
 }
---
+++ src/test/scala/io/chrisdavenport/scalawithcats/chapter8/UptimeClientSpec.scala
@@ -0,0 +1,8 @@
+package io.chrisdavenport.scalawithcats.chapter8
+
+import org.specs2._
+
+
+class UptimeClientSpec {
+
+}
